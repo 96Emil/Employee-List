c92f9241407e1ae5848e12b1e9f68e48eb062da5
96Emil <91869024+96Emil@users.noreply.github.com>
2022-09-22 16:49:43 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import PageOne from "./Components/PageOne";
+import PageTwo from "./Components/PageTwo";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/PageOne" element={<PageOne />} />
+          <Route path="/PageTwo" element={<PageTwo />} />
+        </Routes>
+        <Link className="page-link" to="PageOne">
+          1
+        </Link>
+        <Link className="page-link" to="PageTwo">
+          2
+        </Link>
+      </div>
+    </Router>
   );
 }
 
-export default App;
